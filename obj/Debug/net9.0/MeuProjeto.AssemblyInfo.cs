9c45d0b56ca51af062e44d9aadd96b5b271b9cbb
Daniel <marcosdanielsferreira2015@gmail.com>
2025-07-14 20:41:05 -0300

    User delete

--- obj/Debug/net9.0/MeuProjeto.AssemblyInfo.cs
+++ obj/Debug/net9.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f793a2cd77d9e5af38b2de5f4772cf4bd04bad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad075a040bae9ecc9470ff497f4595901235bf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
